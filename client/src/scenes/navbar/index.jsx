2be10ad85150fed8020445624f9875bef93e6e62
FabKaiz <cacrfabfr@gmail.com>
2022-12-12 09:57:35 +0100

    Changed name of the app

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Coolbook
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
