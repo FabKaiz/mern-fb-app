0738ee5dd31abacf8e56dc7e123afc5dfa3d5d70
FabKaiz <cacrfabfr@gmail.com>
2022-12-12 11:42:52 +0100

    Created Form page with login and register

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material'
+import Form from './Form'
 
 const LoginPage = () => {
+  const theme = useTheme()
+  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)')
   return (
-    <div>LoginPage</div>
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Coolbook
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? '50%' : '93%'}
+        margin="2rem auto"
+        p="2rem"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: '1.5rem' }}>
+          Welcome to Coolbook, the social network for cool people!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
   )
 }
 
