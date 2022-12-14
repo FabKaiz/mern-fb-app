c0ddf4b08850e74f958c6e2616f2dd00fc755303
FabKaiz <cacrfabfr@gmail.com>
2022-12-14 14:12:48 +0100

    fix setPosts typo

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -23,7 +23,7 @@ import FlexBetween from '../../components/FlexBetween'
 import UserImage from '../../components/UserImage'
 import WidgetWrapper from '../../components/WidgetWrapper'
 import { useDispatch, useSelector } from 'react-redux'
-import { setPost } from '../../state'
+import { setPosts } from '../../state'
 
 const MyPostWidget = ({ picturePath }) => {
   const dispatch = useDispatch()
@@ -53,7 +53,7 @@ const MyPostWidget = ({ picturePath }) => {
     })
 
     const posts = await response.json()
-    dispatch(setPost({ posts }))
+    dispatch(setPosts({ posts }))
     setImage(null)
     setPost('')
   }
